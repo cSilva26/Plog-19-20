466eb08fd5c4237f49a5015d22bd7cf2eaf49a92
vteixeira95 <49062252+vteixeira95@users.noreply.github.com>
2019-11-17 14:54:22 +0000

    jogo

--- tp1.pl
+++ tp1.pl
@@ -1,4 +1,4 @@
-:-use_module(library(between)).
+/*:-use_module(library(between)).*/
 
 initial_board(board(Red, Blue)):-
 		Red = [piece(white, 0, 0), piece(black, 1, 0), piece(white, 2, 0), 
@@ -54,6 +54,8 @@ player(blue).
 other_player(red, blue).
 other_player(blue, red).
 
+look_ahead_on(2).
+
 coordinate(X) :- list_member([0, 1, 2, 3, 4], X).
 
 /* Any valid cell on the board.*/
@@ -63,11 +65,26 @@ position(X, Y) :-
     cell(X, Y).
 
 
+evaluate_board(board(Red, Blue), Score) :-
+    list_count(Blue, piece(_, _, _), Blues),
+    list_count(Red, piece(_, _, _), Reds),
+    Score is (Reds-Blues).
+
 /*---------------------------------------------------------------------------------------------*/
+piece_color(NewListCurrentPlayerMoves, X1,Y1, Color):-
+  list_member(NewListCurrentPlayerMoves,piece(Color,X1,Y1)).
+
 list_member(List, Member):- member(Member, List).
 list_absent(List, Element):- \+ (member(Element, List)).
 delete_from_list(List, Element, NewList):- select(Element, List, NewList).
 append_list(List1, List2, Result):- append(List1, List2, Result).
+
+list_count([], _, 0).
+list_count([FirstElement | Tail], Element, Occurrences) :-
+    unifiable(FirstElement, Element, _),
+    list_count(Tail, Element, Tmp),
+    Occurrences is (Tmp + 1), !.
+list_count([_ | Tail], Element, Occurrences) :- list_count(Tail, Element, Occurrences).
 
 /*predicates to list the members of the board depending on the side of the player*/
 listBoards(blue, board(Red, Blue), Blue, Red).
@@ -112,25 +129,171 @@ print_board(Board) :-
     write('    0    1    2    3    4   \n'), !.
 
 /*Movements ------------------------------------------------------------------*/
+/*Movements ------------------------------------------------------------------*/
 move(Side, board(Red, Blue), board(NewRed, NewBlue)) :-
     move_figure(Side, board(Red, Blue), board(NewRed, NewBlue)).
 
 /*predicado que acede à lista de pecas remove a peca escolhida do sitio prévio */
 /*  e adiciona uma nova com a nova posicao escolhida*/
 move_figure(Side, ListPlayer, ListOpponent, NewListPlayer):-
+            delete_from_list(ListPlayer, InitialPosition, TmpListPlayer),
+            possible_move(Side, InitialPosition, FinalPosition),
+            way_is_free(Side, ListPlayer, ListOpponent, FinalPosition, NewListOpponent),
+            append_list(TmpListPlayer, [FinalPosition], NewListPlayer).
+
 
 move_figure(red, board(Red, Blue), board(NewRed, Blue)) :-
     move_figure(red, Red, Blue, NewRed).
 move_figure(blue, board(Red, Blue), board(Red, NewBlue)) :-
     move_figure(blue, Blue, Red, NewBlue).
 
-possible_move(red,piece(ColorPiece,X,Y), piece(_,X2,Y2))):-
+possible_move(red,piece(ColorPiece,X,Y), piece(_,X2,Y2)):-
   board_tile(ColorBoard, X, Y),
+  (((Y2 is Y+1); (Y2 is Y-1); (Y2 is Y)),
+  ((X2 is X+1); (X2 is X-1); (X2 is X)),
+  inside_board(X2,Y2));
+  (ColorBoard==ColorPiece) -> (knight_movement_possible(X, Y, X2, Y2)); (bishop_movement_possible(X, Y, X2, Y2)).
+
+possible_move(blue,piece(ColorPiece,X,Y), piece(_,X2,Y2)):-
+  board_tile(ColorBoard, X, Y),
+  (((Y2 is Y+1); (Y2 is Y-1); (Y2 is Y)),
+  ((X2 is X+1); (X2 is X-1); (X2 is X)),
+  inside_board(X2,Y2));
+  (ColorBoard==ColorPiece) -> (knight_movement_possible(X, Y, X2, Y2)); (bishop_movement_possible(X, Y, X2, Y2)).
+
+knight_movement_possible(Xs,Ys,Xd,Yd):-
+        ground(Xs),ground(Ys),
+        (knight_mov(DistX,DistY);knight_mov(DistY,DistX)),
+        Xd is Xs+DistX,
+        Yd is Ys+DistY,
+        inside_board(Xd,Yd).
+
+bishop_movement_possible(Xi,Yi,Xf,Yf):-
+        Xi \= Xf, Yi \= Yf,
+        DiagDiffi is (Xi-Yi), DiagDifff is (Xf-Yf),
+        DiagSumi is (Xi+Yi), DiagSumf is (Xf+Yf),
+        (DiagDiffi = DiagDifff ; DiagSumi = DiagSumf),
+        inside_board(Xf,Yf).
 
 
-inside_board(X/Y):-
+move_is_possible(red,piece(ColorPiece,X,Y), piece(_,X2,Y2)):-
+  inside_board(X2,Y2),
+  ((board_tile(ColorBoard, X, Y),
+  (ColorBoard==ColorPiece) -> (bishop_movement(X, Y, X2, Y2)) ; (knight_movement(X, Y, X2, Y2)));
+  (
+    (((Y2=:=Y+1); (Y2=:=Y-1); (Y2=:=Y)),((X2=:=X+1); (X2=:=X-1) ; (X2=:=X))),
+  inside_board(X2,Y2)
+  )).
+
+knight_movement(Xs, Ys, Xd, Yd):-
+        ground(Xs),ground(Ys),
+        (knight_mov(DistX,DistY);knight_mov(DistY,DistX)),
+        (Xd =:= Xs+DistX;
+        Yd =:= Ys+DistY),
+        inside_board(Xd,Yd).
+
+bishop_movement(Xi, Yi, Xf, Yf):-
+        Xi \= Xf, Yi \= Yf,
+        DiagDiffi is (Xi-Yi), DiagDifff is (Xf-Yf),
+        DiagSumi is (Xi+Yi), DiagSumf is (Xf+Yf),
+        (DiagDiffi = DiagDifff ; DiagSumi = DiagSumf),
+        inside_board(Xf,Yf).
+
+inside_board(X,Y):-
       between(0,4,X),
       between(0,4,Y).
+
+knight_mov(-2,-1).
+knight_mov(-2,1).
+knight_mov(2,-1).
+knight_mov(2,1).
+
+/* Predicate to check if the way is free */
+way_is_free(Side, ListCurrentPlayerMoves, ListNoMoves, piece(_, X2, Y2), NewListOpponent) :-
+    inside_board(X2,Y2),
+    (list_member(ListNoMoves, piece(_, X2, Y2)),
+      delete_from_list(ListNoMoves, piece(_,X2,Y2), NewListOpponent)
+      );
+    (list_absent(ListCurrentPlayerMoves, piece(_, X2, Y2)),
+      NewListOpponent = ListNoMoves).
+/*---------------MINMAX Algorithm---------------------------------------------*/
+swap_node_type(min, max).
+swap_node_type(max, min).
+
+/* Helper predicate to get board with best score. For max node 'best' is maximal score. For min node - minimal.*/
+best_score_and_board(max, Board1, Score1, Board2, Score2, BestBoard, BestScore) :-
+    (Score1 >= Score2, !, BestBoard = Board1, BestScore = Score1) ;
+    (Score1  < Score2, BestBoard = Board2, BestScore = Score2).
+best_score_and_board(min, Board1, Score1, Board2, Score2, BestBoard, BestScore) :-
+    (Score1 =< Score2, !, BestBoard = Board1, BestScore = Score1) ;
+    (Score1  > Score2, BestBoard = Board2, BestScore = Score2).
+
+/* Checks if specified value prune other nodes.*/
+alpha_beta_prune(max, _, Beta, Value)  :- Value > Beta.
+alpha_beta_prune(min, Alpha, _, Value) :- Value < Alpha.
+
+/*Update Alpha or Beta value based on new 'Value'*/
+update_alpha_beta(max, Alpha, Beta, Value, NewAlpha, Beta) :- (Value > Alpha, !, NewAlpha = Value) ; (NewAlpha = Alpha).
+update_alpha_beta(min, Alpha, Beta, Value, Alpha, NewBeta) :- (Value < Beta,  !, NewBeta = Value) ; (NewBeta = Beta).
+
+/* Build list of all possible moves for the 'Side' on provided board.*/
+possible_moves_list(Side, Board, List) :- findall(NewBoard, move(Side, Board, NewBoard), List).
+
+/* Get best move from the list of possible moves.*/
+/* If depth is zero (leaf node), choose board by the best score.*/
+find_best_move(_, _, [BestBoard], BestBoard, BestScore, 0, _, _) :-
+    evaluate_board(BestBoard, BestScore), !.
+
+find_best_move(Side, NodeType, [Board1 | Tail], BestBoard, BestScore, 0, Alpha, Beta) :-
+    evaluate_board(Board1, Score1),
+    (
+        (alpha_beta_prune(NodeType, Alpha, Beta, Score1), !, BestBoard = Board1, BestScore = Score1) ;
+        (
+            find_best_move(Side, NodeType, Tail, Board2, Score2, 0, Alpha, Beta),
+            best_score_and_board(NodeType, Board1, Score1, Board2, Score2, BestBoard, BestScore)
+        )
+    ).
+
+/* If depth is not zero - evaluate each node with minmax predicate, and chose the best.*/
+find_best_move(Side, NodeType, [BestBoard], BestBoard, BestScore, Depth, Alpha, Beta) :-
+    Depth > 0,
+    NewDepth is (Depth - 1),
+    swap_node_type(NodeType, NextNodeType),
+    other_player(Side, OtherSide),
+    minmax(OtherSide, NextNodeType, BestBoard, _, BestScore, NewDepth, Alpha, Beta), !.
+
+find_best_move(Side, NodeType, [Board1 | Tail], BestBoard, BestScore, Depth, Alpha, Beta) :-
+    Depth > 0,
+    NewDepth is (Depth - 1),
+    swap_node_type(NodeType, NextNodeType),
+    other_player(Side, OtherSide),
+    minmax(OtherSide, NextNodeType, Board1, _, Score1, NewDepth, Alpha, Beta),
+    (
+        (alpha_beta_prune(NodeType, Alpha, Beta, Score1), !, BestBoard = Board1, BestScore = Score1) ;
+        (
+            update_alpha_beta(NodeType, Alpha, Beta, Score1, NewAlpha, NewBeta),
+            find_best_move(Side, NodeType, Tail, Board2, Score2, Depth, NewAlpha, NewBeta),
+            best_score_and_board(NodeType, Board1, Score1, Board2, Score2, BestBoard, BestScore)
+        )
+    ).
+
+/* If there are no more possible moves: this is a leaf node. Only need to evaluate it.*/
+minmax(Side, _, Board, Board, Score, _, _, _) :-
+    possible_moves_list(Side, Board, []),
+    evaluate_board(Board, Score), !.
+
+/* Otherwise get list of all possible moves and choose the best.*/
+minmax(Side, NodeType, Board, BestBoard, BestScore, Depth, Alpha, Beta) :-
+    possible_moves_list(Side, Board, PossibleMoves),
+    find_best_move(Side, NodeType, PossibleMoves, BestBoard, BestScore, Depth, Alpha, Beta).
+
+/* Predicate for 'smart move' for each side.*/
+smart_move(red, Board, NewBoard) :-
+    look_ahead_on(Depth), minmax(red, max, Board, NewBoard, _, Depth, -100000, 100000).
+smart_move(blue, Board, NewBoard) :-
+    look_ahead_on(Depth), minmax(blue, min, Board, NewBoard, _, Depth, -100000, 100000).
+
+
 
 /*User Input ------------------------------------------------------------------*/
 
@@ -143,32 +306,48 @@ player_select_side(Side) :-
         (write('Invalid side, please type \'red.\' or \'blue.\'\n'), player_select_side(Side))
     ).
 
+player_input_pos(position(X, Y)) :-
+    read(A-B),
+    (
+        (position(A, B), X = A, Y = B, !);
+        (write('Invalid position. Retry: '), player_input_pos(position(X, Y)))
+    ).
+
 /*predicate to request player to input a movement*/
 player_input_move(Side, board(Red, Blue), NewBoard):- 
     write('Choose piece to move: (X-Y)'),
     player_input_pos(position(X1, Y1)), 
-    write('Choose where to move: (X-Y)')
+    write('Choose where to move: (X-Y)'),
     player_input_pos(position(X2, Y2)),
-    listBoards(Side, board(Red, Blue), ListCurrentPlayerMoves, ListNoMoves),
+    listBoards(Side, board(Red, Blue), ListCurrentPlayerMoves, ListOpponent),
     (
-      (delete_from_list(ListCurrentPlayerMoves, position(X1,Y1), ListCurrentPlayerMOvesTemp),
-       append_list(ListCurrentPlayerMovesTemp, position(X2, Y2), NewListCurrentPLayerMoves), 
-       listBoards(Side, NewBoard, NewListCurrentPLayerMoves, ListNoMoves),
-       move(Side, board(Red, Blue), NewBoard)
+      (listBoards(Side, NewBoard, NewListCurrentPlayerMoves, NewListOpponent),
+       /*possible_move(Side, piece(_,X1,Y1), piece(_,X2,Y2)),*/
+       piece_color(ListCurrentPlayerMoves, X1,Y1, Color),
+       move_is_possible(Side,piece(Color,X1,Y1), piece(_,X2,Y2)),
+       way_is_free(Side, ListCurrentPlayerMoves, ListOpponent, piece(_,X2,Y2), NewListOpponent),
+       delete_from_list(ListCurrentPlayerMoves, piece(Color,X1,Y1), ListCurrentPlayerMovesTemp),
+       append_list(ListCurrentPlayerMovesTemp, [piece(Color,X2, Y2)], NewListCurrentPlayerMoves)
+       /*move(Side, board(Red, Blue), NewBoard, piece(Color,X1,Y1), piece(_,X2,Y2))*/
         ); 
       write('That move is not valid. Try again: (X-Y)'), player_input_move(Side, board(Red, Blue), NewBoard)
     ),!.
 /*------------------------------------------------------------------------------*/
 game(Side, Board1):-
-        otherSide(Side, otherSide),
+        other_player(Side, OtherSide),
         print_board(Board1),
-        /*avaliacao_estado(),*/
+        /*avaliacao_estado(Side, Board1),*/
         player_input_move(Side, Board1, Board2),
+        write('\n'), write(Side), write(' moving:\n'),
+        print_board(Board2),
+        /*can_move(OtherSide, Board2),*/
+        smart_move(OtherSide, Board2, Board3),
+        write('\n'), write(OtherSide), write(' moving:\n'),
+        game(Side, Board3).
 
 
 
-draw:-
+play:-
 	initial_board(B),
-  /*sample2(B), */
   player_select_side(Side),
-	print_board(B).
+	game(Side, B).
